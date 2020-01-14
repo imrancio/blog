abc8a5b1601034c00d3b914c3ebc853f02f23793
Imran Chowdhury <imran@imranc.io>
2020-01-15 01:13:23 +1100

    Added autolink headers for markdown table of contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,12 @@ module.exports = {
 							wrapperStyle: `margin-bottom: 1.0725rem`,
 						},
 					},
+					{
+						resolve: `gatsby-remark-autolink-headers`,
+						options: {
+							icon: ``,
+						},
+					},
 					`gatsby-remark-prismjs`,
 					`gatsby-remark-copy-linked-files`,
 					`gatsby-remark-smartypants`,
