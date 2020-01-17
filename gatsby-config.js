85b58da61e6d01baa2e20f7e77da6bd4bf0c8902
Imran Chowdhury <imran@imranc.io>
2020-01-18 02:44:51 +1100

    Updated Google Analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,8 +125,6 @@ module.exports = {
 				// This object gets passed directly to the gtag config command
 				// This config will be shared across all trackingIds
 				gtagConfig: {
-					optimize_id: 'OPT_CONTAINER_ID',
-					anonymize_ip: true,
 					cookie_expires: 0,
 				},
 				// This object is used for configuration specific to this plugin
