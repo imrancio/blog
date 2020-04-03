dc7f3c1eee34e48a53d8184e08ead2d81018a7ad
Divyanshu Maithani <div.blackcat@gmail.com>
2020-04-03 18:43:49 +0530

    Update newsletter

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,15 +122,15 @@ module.exports = {
 				feeds: [
 					{
 						serialize: ({ query: { site, allMarkdownRemark } }) => {
-							return allMarkdownRemark.nodes.map(node => {
+							return allMarkdownRemark.nodes.map((node) => {
 								return Object.assign({}, node.frontmatter, {
 									description: node.excerpt,
 									date: node.frontmatter.date,
 									url: site.siteMetadata.siteUrl + node.fields.slug,
 									guid: site.siteMetadata.siteUrl + node.fields.slug,
-									custom_elements: [{ "content:encoded": node.html }],
-								})
-							})
+									custom_elements: [{ 'content:encoded': node.html }],
+								});
+							});
 						},
 						query: `
 					{
@@ -140,8 +140,8 @@ module.exports = {
 						nodes {
 						  excerpt
 						  html
-						  fields { 
-							slug 
+						  fields {
+							slug
 						  }
 						  frontmatter {
 							title
@@ -151,13 +151,13 @@ module.exports = {
 					  }
 					}
 				  `,
-						output: "/rss.xml",
-						title: "Imran C’s RSS Feed",
+						output: '/rss.xml',
+						title: 'Imran C’s RSS Feed',
 						// optional configuration to insert feed reference in pages:
 						// if `string` is used, it will be used to create RegExp and then test if pathname of
 						// current page satisfied this regular expression;
 						// if not provided or `undefined`, all pages will have feed reference inserted
-						match: "^/posts/",
+						match: '^/posts/',
 					},
 				],
 			},
