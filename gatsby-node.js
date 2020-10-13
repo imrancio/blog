1f1c0c826b257c32ef33ed1f93dcdc6e78d4b866
Divyanshu Maithani <div.blackcat@gmail.com>
2020-10-13 23:30:41 +0530

    Configure about page query and template

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,7 @@ exports.createPages = ({ graphql, actions }) => {
 		`
 			{
 				postsRemark: allMarkdownRemark(
+					filter: { frontmatter: { title: { ne: "About" } } }
 					sort: { fields: [frontmatter___date], order: DESC }
 					limit: 1000
 				) {
--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { OutboundLink } from 'gatsby-plugin-google-gtag';
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 import { mediaMax } from '@divyanshu013/media';
 import { rhythm } from '../utils/typography';
@@ -9,29 +9,30 @@ import { getTheme } from '../utils/theme';
 import ThemeContext from './ThemeContext';
 
 const Bio = () => {
-	const data = useStaticQuery(graphql`query BioQuery {
-  avatar: file(absolutePath: {regex: "/profile-pic.png/"}) {
-    childImageSharp {
-      gatsbyImageData(width: 64, height: 64, layout: FIXED)
-    }
-  }
-  site {
-    siteMetadata {
-      author
-      social {
-        github
-        linkedin
-      }
-    }
-  }
-}
-`);
+	const data = useStaticQuery(graphql`
+		query BioQuery {
+			avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
+				childImageSharp {
+					gatsbyImageData(width: 64, height: 64, layout: FIXED)
+				}
+			}
+			site {
+				siteMetadata {
+					author
+					social {
+						github
+						linkedin
+					}
+				}
+			}
+		}
+	`);
 
 	const { author, social } = data.site.siteMetadata;
 	const { theme } = useContext(ThemeContext);
 	const { color, secondary } = getTheme(theme);
 	return (
-        <div
+		<div
 			css={{
 				display: `grid`,
 				gridTemplateColumns: 'auto auto',
@@ -49,9 +50,9 @@ const Bio = () => {
 			}}
 		>
 			<GatsbyImage
-                image={data.avatar.childImageSharp.gatsbyImageData}
-                alt={author}
-                css={{
+				image={data.avatar.childImageSharp.gatsbyImageData}
+				alt={author}
+				css={{
 					marginTop: 8,
 					marginRight: rhythm(1),
 					borderRadius: `100%`,
@@ -61,9 +62,10 @@ const Bio = () => {
 					},
 					backgroundColor: `none`,
 				}}
-                imgStyle={{
+				imgStyle={{
 					borderRadius: `50%`,
-				}} />
+				}}
+			/>
 			<div css={{ fontSize: 16, color: secondary }}>
 				<p>
 					Personal blog of <OutboundLink href={social.linkedin}>{author}</OutboundLink>. I’m a{' '}
@@ -74,7 +76,7 @@ const Bio = () => {
 				</p>
 			</div>
 		</div>
-    );
+	);
 };
 
 export default Bio;
