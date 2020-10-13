c8b06a3e0fbaab0a79552eb00542be7f8fc02195
Divyanshu Maithani <div.blackcat@gmail.com>
2020-10-14 00:15:07 +0530

    Add font loading for 700

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,7 @@
 import './src/styles/global.css';
 // custom typefaces
 import 'fontsource-open-sans';
+import 'fontsource-open-sans/700.css';
 import 'fontsource-zilla-slab';
+import 'fontsource-zilla-slab/700.css';
 import 'fontsource-cascadia-code';
