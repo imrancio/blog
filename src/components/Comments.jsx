77a3eda9121975dbb20150a0a2d835ed21c4111a
Imran Chowdhury <imran@imranc.io>
2022-05-24 17:04:06 +0600

    chore: fix component errors

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,38 +3,37 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import ThemeContext from './ThemeContext';
 
-const commentNodeId = 'comments';
-
-const Comments = () => {
-	const data = useStaticQuery(graphql`
-		query RepoQuery {
-			site {
-				siteMetadata {
-					repo
-				}
+const REPO_QUERY = graphql`
+	query RepoQuery {
+		site {
+			siteMetadata {
+				repo
 			}
 		}
-	`);
+	}
+`;
+
+const Comments = () => {
+	const data = useStaticQuery(REPO_QUERY);
+	const repo = data.site.siteMetadata.repo;
 	const { theme } = useContext(ThemeContext);
 
-	useEffect(() => {
-		const script = document.createElement('script');
-		script.src = 'https://utteranc.es/client.js';
-		script.async = true;
-		script.setAttribute('repo', data.site.siteMetadata.repo);
-		script.setAttribute('issue-term', 'pathname');
-		script.setAttribute('label', 'comment :speech_balloon:');
-		script.setAttribute('theme', theme === 'dark' ? 'github-dark' : 'github-light');
-		script.setAttribute('crossorigin', 'anonymous');
-
-		const scriptParentNode = document.getElementById(commentNodeId);
-		if (scriptParentNode.firstChild) {
-			scriptParentNode.removeChild(scriptParentNode.firstChild);
-		}
-		scriptParentNode.appendChild(script);
-	}, [data, theme]);
-
-	return <div id={commentNodeId} />;
+	return (
+		<section
+			ref={(element) => {
+				if (!element) return;
+				const script = document.createElement('script');
+				script.src = 'https://utteranc.es/client.js';
+				script.async = true;
+				script.setAttribute('repo', repo);
+				script.setAttribute('issue-term', 'pathname');
+				script.setAttribute('label', 'comment :speech_balloon:');
+				script.setAttribute('theme', theme === 'dark' ? 'github-dark' : 'github-light');
+				script.setAttribute('crossorigin', 'anonymous');
+				element.replaceChildren(script);
+			}}
+		/>
+	);
 };
 
 export default Comments;
