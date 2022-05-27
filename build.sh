c5285809eba9005eee33c6fc4fd31d1d2b7e2a34
Imran Chowdhury <imran@imranc.io>
2022-05-27 19:22:50 +0600

    ci: update dockerfile

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
-
-# build gatsby image with plugins
-docker build -t gatsbyjs:blog -f Dockerfile.gatsby .
-# build blog image from gatsby image
-docker build -t imrancio-blog:latest .
+docker build -t imrancio/blog:latest .
