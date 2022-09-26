21394385e3d75b98397840ab158dafdde6b34286
Imran Chowdhury <imran@imranc.io>
2022-09-26 18:24:00 +1000

    chore: generate types on build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,7 +26,10 @@ const gatsbyConfig: GatsbyConfig = {
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen
-	graphqlTypegen: true,
+	graphqlTypegen: {
+		typesOutputPath: `src/gatsby-types.d.ts`,
+		generateOnBuild: true,
+	},
 	plugins: [
 		{
 			resolve: `gatsby-plugin-google-gtag`,
