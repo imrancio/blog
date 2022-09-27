b7739a8950ba54d6b892d8e70113f06aee6fb155
Imran Chowdhury <imran@imranc.io>
2022-09-27 22:09:22 +1000

    ci: fix ts type-check

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,15 +1,1 @@
 declare const __PATH_PREFIX__: string;
-declare global {
-	namespace NodeJS {
-		interface ProcessEnv {
-			/** Node.js environment */
-			NODE_ENV: 'development' | 'production' | 'test';
-			/** Disable webpack bundle analyser during build */
-			DISABLE_WEBPACK_ANALYSER: boolean;
-		}
-	}
-}
-
-// If this file has no import/export statements (i.e. is a script)
-// convert it into a module by adding an empty export statement.
-export {};
