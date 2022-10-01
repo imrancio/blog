f3a3e2231218e21cd1398c294b8b10c06c5c0715
Imran Chowdhury <imran@imranc.io>
2022-10-01 10:59:21 +1000

    chore(types): update ts types

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -5,36 +5,42 @@ import BlogInfo from './BlogInfo';
 import BlogPreview from './BlogPreview';
 import { rhythm } from '../utils/typography';
 
-const BlogList = ({ posts }) => {
+type BlogListProps = {
+	posts: Queries.BlogIndexQuery['allMarkdownRemark']['nodes'];
+};
+
+const BlogList = ({ posts }: BlogListProps) => {
 	const indexName = process.env.GATSBY_ALGOLIA_INDEX_NAME;
 	const { uiState } = useInstantSearch();
 	return uiState[indexName].query ? (
 		<Hits hitComponent={BlogPreview} />
 	) : (
-		posts.map((node) => {
-			const title = node.frontmatter.title || node.fields.slug;
-			return (
-				<div key={node.fields.slug}>
-					<BlogInfo timeToRead={node.timeToRead} date={node.frontmatter.date} />
-					<h3
-						css={{
-							marginTop: rhythm(1 / 4),
-							marginBottom: rhythm(0.5),
-						}}
-					>
-						<Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-							{title}
-						</Link>
-					</h3>
-					<p
-						css={{ marginBottom: rhythm(1.5) }}
-						dangerouslySetInnerHTML={{
-							__html: node.frontmatter.description || node.excerpt,
-						}}
-					/>
-				</div>
-			);
-		})
+		<>
+			{posts.map((node) => {
+				const title = node.frontmatter.title || node.fields.slug;
+				return (
+					<div key={node.fields.slug}>
+						<BlogInfo timeToRead={node.timeToRead} date={node.frontmatter.date} />
+						<h3
+							css={{
+								marginTop: rhythm(1 / 4),
+								marginBottom: rhythm(0.5),
+							}}
+						>
+							<Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+								{title}
+							</Link>
+						</h3>
+						<p
+							css={{ marginBottom: rhythm(1.5) }}
+							dangerouslySetInnerHTML={{
+								__html: node.frontmatter.description || node.excerpt,
+							}}
+						/>
+					</div>
+				);
+			})}
+		</>
 	);
 };
 
--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { Highlight, Snippet } from 'react-instantsearch-hooks-web';
+import type { Hit } from 'instantsearch.js';
+import type { SendEventForHits } from 'instantsearch.js/es/lib/utils';
+
 import BlogInfo from './BlogInfo';
 import { rhythm } from '../utils/typography';
 
-const BlogPreview = ({ hit }) => {
+/** see [gatsby-plugin-algolia](../../gatsby-config.ts) transformer output */
+export type BaseHit = {
+	id: string;
+	slug: string;
+	date: string;
+	title: string;
+	description: string;
+	excerpt: string;
+	timeToRead: number;
+};
+
+type BlogPreviewProps = {
+	hit: Hit<BaseHit>;
+	sendEvent: SendEventForHits;
+};
+
+const BlogPreview = ({ hit }: BlogPreviewProps) => {
 	return (
 		<div key={hit.slug}>
 			<BlogInfo timeToRead={hit.timeToRead} date={hit.date} />
@@ -19,7 +38,8 @@ const BlogPreview = ({ hit }) => {
 				</Link>
 			</h3>
 			<p css={{ marginBottom: rhythm(1) }}>
-				{hit._highlightResult.description.matchLevel === 'none' ? (
+				{!Array.isArray(hit._highlightResult.description) &&
+				hit._highlightResult.description.matchLevel === 'none' ? (
 					<Snippet hit={hit} attribute="excerpt" highlightedTagName="mark" />
 				) : (
 					<Highlight hit={hit} attribute="description" highlightedTagName="mark" />
