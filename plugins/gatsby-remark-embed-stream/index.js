637aa83ea1351c185091b032b58bb3963e5595e7
Imran Chowdhury <imran@imranc.io>
2024-05-18 21:34:01 +1000

    fix: eslint excludes dist files

--- plugins/gatsby-remark-embed-stream/index.js
+++ plugins/gatsby-remark-embed-stream/index.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 /**
  * Gatsby plugins always reference from './' thus files in ./dist were not found.
  * Thus to avoid any issues with how Gatsby works this file was added
-*/
+ */
 
-module.exports = require("./dist/index");
+module.exports = require('./dist/index');
